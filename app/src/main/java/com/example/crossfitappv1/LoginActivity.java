30c77eec72a971c5aad89270f03ca76e1cff715c
yasiru1999 <it20255510@my.sliit.lk>
2021-09-20 07:28:36 +0530

    Yasiru first commit

--- app/src/main/java/com/example/crossfitappv1/LoginActivity.java
+++ app/src/main/java/com/example/crossfitappv1/LoginActivity.java
@@ -1,22 +1,71 @@
 package com.example.crossfitappv1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView registerlink;
+    Button btnLogin;
+    EditText email,password;
+    boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        email = findViewById(R.id.editTxtRegFullName);
+        password = findViewById(R.id.editTextLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
         registerlink = (TextView) findViewById(R.id.txtRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(email);
+                checkField(password);
+
+                if (valid){
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                            Toast.makeText(LoginActivity.this,"LoggedIn Successfully",Toast.LENGTH_SHORT).show();
+                            checkUserAccessLevel(authResult.getUser().getUid());
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+            }
+        });
 
         registerlink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,9 +75,49 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void checkUserAccessLevel(String uid) {
+        DocumentReference df = fStore.collection("Users").document(uid);
+        //extract the data from the document
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG","onSuccess:" + documentSnapshot.getData());
+                //identify the user access level
+
+                if (documentSnapshot.getString("isUser") != null){
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                    finish();
+                }
+                if (documentSnapshot.getString("isAdmin") != null) {
+                    //user is admin
+                    startActivity(new Intent(getApplicationContext(), AdminHome.class));
+                    finish();
+                }
+            }
+        });
+    }
+
+    public boolean checkField(EditText textField) {
+        if (textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+        return valid;
+    }
+
     public void registerPage() {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
+    /*@Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            finish();
+        }
+    }*/
 }
--- app/src/main/java/com/example/crossfitappv1/RegisterActivity.java
+++ app/src/main/java/com/example/crossfitappv1/RegisterActivity.java
@@ -1,14 +1,137 @@
 package com.example.crossfitappv1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText fullname,email,password,dob,CurWeight,GoalWeight,height;
+    Button signup;
+    TextView Login;
+    RadioButton isMale,isFemale;
+    boolean valid;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        fullname = findViewById(R.id.editTxtRegFullName);
+        email = findViewById(R.id.editTxtEmail);
+        password = findViewById(R.id.editTxtPassword);
+        dob = findViewById(R.id.editTxtDOB);
+        CurWeight = findViewById(R.id.etCWeight);
+        GoalWeight = findViewById(R.id.etGWeight);
+        height = findViewById(R.id.etHeight);
+        isMale = findViewById(R.id.radioBtnMale);
+        isFemale = findViewById(R.id.radioBtnFemale);
+        signup = findViewById(R.id.btnSignUp);
+        Login = findViewById(R.id.RegToLogin);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(fullname);
+                checkField(email);
+                checkField(password);
+                checkField(dob);
+                checkField(CurWeight);
+                checkField(GoalWeight);
+                checkField(height);
+
+                //radiobutton validation
+                if( ! (isMale.isChecked() || isFemale.isChecked())){
+                    Toast.makeText(RegisterActivity.this,"Select the geneder",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (valid){
+
+                    //User Registration process
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this,"Account Created",Toast.LENGTH_SHORT).show();
+                            DocumentReference df = fStore.collection("Users").document(user.getUid());
+                            Map<String,Object> userInfo = new HashMap<>();
+                            userInfo.put("FullName",fullname.getText().toString());
+                            userInfo.put("Email",email.getText().toString());
+                            userInfo.put("dob",dob.getText().toString());
+                            userInfo.put("CurWeight",CurWeight.getText().toString());
+                            userInfo.put("GoalWeight",GoalWeight.getText().toString());
+                            userInfo.put("height",height.getText().toString());
+
+                            if (isMale.isChecked()){
+                                userInfo.put("isMale","1");
+                            }
+                            if (isFemale.isChecked()){
+                                userInfo.put("isFemale","1");
+                            }
+
+                            //specify if the user is admin
+                            userInfo.put("isUser","1");
+
+                            df.set(userInfo);
+
+                            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                            finish();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(RegisterActivity.this,"Failed to Create",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
     }
+
+
+    private boolean checkField(EditText textField) {
+        if (textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else
+        {
+            valid = true;
+        }
+        return valid;
+    }
+
 }
