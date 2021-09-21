3756b2009d7d022172f31b4d6c0712327d08cf0a
Dulakshi Rathnayake <87512180+DulakshiRathnayake@users.noreply.github.com>
2021-09-21 22:39:56 +0530

    Dulakshi category(admin)

---
+++ app/src/main/java/com/example/crossfitappv1/AdminCategory.java
@@ -0,0 +1,29 @@
+package com.example.crossfitappv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class AdminCategory extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminCategory.this,AddCategory.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/crossfitappv1/HomeActivity.java
+++ app/src/main/java/com/example/crossfitappv1/HomeActivity.java
@@ -1,16 +1,34 @@
 package com.example.crossfitappv1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    public CardView card1;
 
-    //comment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        card1 = (CardView) findViewById(R.id.package_card);
+
+        card1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.package_card:
+                i = new Intent(this, AdminCategory.class);
+                startActivity(i);
+                break;
+        }
     }
 }
